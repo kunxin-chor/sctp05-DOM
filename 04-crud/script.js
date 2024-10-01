dd5fb3249d6e7f33b75cd24a182048c6f71b57a3
kunxin-chor <chorkunxin@yahoo.com>
2024-10-01 02:34:57 +0000

    Added C of the CRUD

---
+++ 04-crud/script.js
@@ -0,0 +1,51 @@
+// need a way to store our data
+let tasks = [
+    {
+        "id": 1,  // unique identifier for the record
+        "name": "Wash the car",
+        "urgency": 3,
+        "done": false
+    },
+    {
+        "id": 2,
+        "name": "Buy groceries",
+        "urgency":5,
+        "done": false
+    },
+    {
+        "id": 3,
+        "name": "Wash the toilet",
+        "urgency":2,
+        "done": true
+    }
+]
+// DOMContentLoaded is an event that fires
+// when the HTML is loaded completely
+window.addEventListener("DOMContentLoaded", function(){
+    // render all the tasks in the database
+    renderTasks();
+
+})
+
+// renderTasks is to update the ul#tasks with 
+// all the tasks
+function renderTasks() {
+
+    let taskList = document.querySelector("#tasks");
+
+    for (let t of tasks) {
+        let liElement = document.createElement("li");
+        liElement.innerHTML = `
+            ${t.name} (${t.urgency}) 
+        `
+
+        // Using JS to write <input type="checkbox"/>
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = t.done;  // if task is done, checkbox will be checked
+
+        liElement.appendChild(checkbox);
+
+        taskList.appendChild(liElement);
+    }
+}
